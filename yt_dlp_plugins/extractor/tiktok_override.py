7695d4b03504b15579a8cea30f5cc804c2bff574
demon071 <anhk1996@gmail.com>
2023-11-09 22:14:42 +0700

    update

---
+++ yt_dlp_plugins/extractor/tiktok_override.py
@@ -0,0 +1,12 @@
+from yt_dlp.extractor.tiktok import TikTokBaseIE
+
+# ⚠ Other plugins cannot be overridden using this method
+# ⚠ The extractor internals may change without warning, breaking the plugin
+
+
+
+class _TikTokOverrideTikTokIE(TikTokBaseIE, plugin_name='tiktok'):
+    def _get_sigi_state(self, webpage, display_id):
+        return self._search_json(
+            r'<script[^>]+\bid="(?:SIGI_STATE|sigi-persisted-data|__UNIVERSAL_DATA_FOR_REHYDRATION__)"[^>]*>', webpage,
+            'sigi state', display_id, end_pattern=r'</script>')
